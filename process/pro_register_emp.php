4718a4400669d9234cb51bf00a0a7049f8058566
Guilherme <Guilhermev166@outlook.com>
2025-07-22 14:56:49 -0300

    deleted:    LICENSE 	deleted:    README.md 	deleted:    index.html 	deleted:    logo.svg 	new file:   php/config.php 	new file:   process/pro_register_emp.php 	new file:   process/pro_register_fun.php 	deleted:    style.css

---
+++ process/pro_register_emp.php
@@ -0,0 +1,35 @@
+<?php
+include_once('../php/config.php');
+
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+    $cnpj = $_POST['cnpj'];
+    $razao = $_POST['razao'];
+    $fantasia = $_POST['fantasia'];
+    $inscricao = $_POST['inscricao'];
+    $segmento = $_POST['segmento'];
+    $pro_ser = $_POST['pro_ser'];
+    $email = $_POST['email'];
+    $senha = $_POST['senha'];
+
+    try {
+        $stmt = $conn->prepare("INSERT INTO empresas (cnpj, razao_social, nome_fantasia, inscricao_estadual, segmento, produtos_servicos, email_contato, senha_hash) VALUES (:cnpj, :razao, :fantasia, :inscricao, :segmento, :pro_ser, :email, :senha)");
+        $stmt->bindParam(':cnpj', $cnpj);
+        $stmt->bindParam(':razao', $razao);
+        $stmt->bindParam(':fantasia', $fantasia);
+        $stmt->bindParam(':inscricao', $inscricao);
+        $stmt->bindParam(':segmento', $segmento);
+        $stmt->bindParam(':pro_ser', $pro_ser);
+        $stmt->bindParam(':email', $email);
+        $hashed_senha = password_hash($senha, PASSWORD_DEFAULT);
+        $stmt->bindParam(':senha', $hashed_senha);
+        if ($stmt->execute()) {
+            echo "Empresa registrada com sucesso!";
+            header("Location: ../index.html");
+            exit();
+        } else {
+            echo "Erro ao registrar a empresa.";
+        }
+    } catch (PDOException $e) {
+        echo "Erro: " . $e->getMessage();
+    }
+}
