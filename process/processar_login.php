aadddadf6ef9c0746d01aaf61323978b8b818b06
Guilherme <Guilhermev166@outlook.com>
2025-08-19 16:35:37 -0300

    renamed:    Perfil/perfil.html -> Perfil/perfil.php 	modified:   mcheck.sql 	modified:   process/processar_login.php

--- process/processar_login.php
+++ process/processar_login.php
@@ -5,7 +5,7 @@
         $usuario = $_POST['usuario'];
         $senha = $_POST['senha'];
 
-        $sql = "SELECT funcionario_id, nome_completo, senha_hash FROM funcionarios WHERE nome_completo = ?";
+        $sql = "SELECT funcionario_id, nome_completo, senha_hash, email, telefone_celular, cpf FROM funcionarios WHERE nome_completo = ?";
         $stmt = $conn->prepare($sql);
         $stmt->execute([$usuario]);
 
@@ -15,8 +15,11 @@
             $_SESSION['loggedin'] = true;
             $_SESSION['user_id'] = $user['funcionario_id'];
             $_SESSION['username'] = $user['nome_completo'];
+            $_SESSION['email'] = $user['email'];
+            $_SESSION['telefone'] = $user['telefone_celular'];
+            $_SESSION['cpf'] = $user['cpf'];
         
-        header("Location: pagina_restrita.php");
+        header("Location: ../Perfil/perfil.php");
         exit;
         } else {
             $_SESSION['erro_login'] = "Nome de usuário ou senha inválidos.";
