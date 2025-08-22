488920194fb2b773bef2a605fafd6269a1d10ace
CharlesAcioli <charlesacioli1@gmail.com>
2025-08-22 15:46:12 -0300

    .

--- Login MCheck/login.php
+++ Login MCheck/login.php
@@ -2,9 +2,11 @@
 if($_SERVER['REQUEST_METHOD'] === 'POST'){
     if(isset($_POST['entrar']) && $_POST['entrar'] === 'entrar'){
         header("Location:../Relatórios/relatorios.php");
-    }else if(isset($_POST['cadastrar']) && $_POST['cadastrar'] === 'cadastrar'){
-        header("Location:../Cadastro Usuário/cadastro_usuario.php");
-    }
+        exit;
+    }// else if(isset($_POST['cadastrar']) && $_POST['cadastrar'] === 'cadastrar'){
+    //     header("Location:../Cadastro Usuário/cadastro_usuario.php");
+    //     exit;
+    // }
 }
 ?>
 
@@ -14,6 +16,7 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="">
     <title>Pagina de Login</title>
 </head>
 <body>
@@ -24,18 +27,18 @@ if($_SERVER['REQUEST_METHOD'] === 'POST'){
 
             <div class="Usuario">
                 <label for="usuario">Usuário:</label>
-                <input type="text" name="usuario" placeholder="Informe o usuário">
+                <input type="text" name="usuario" placeholder="Informe o usuário" required>
             </div>
 
             <div class="Senha">
                 <label for="senha">Senha:</label>
-                <input type="password" placeholder="Digite sua senha" name="senha">
+                <input type="password" placeholder="Digite sua senha" name="senha" required>
             </div>
             <br>
 
             <div class="acoes">
                 <button type="submit" name="entrar" value="entrar">Login</button>
-                <button type="submit" name="cadastrar" value="cadastrar">Cadastrar</button>
+                <button type="button" onclick="window.location.href='../Cadastro Usuário/cadastro_usuario.php'">Cadastrar</button>
             </div>
         </form>
     </main>
